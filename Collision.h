c8f9c709d9c7ba37f356dbf4caae34dae609ca1e
Nicholas Montefusco <nmontefusco@yahoo.com>
2018-01-06 08:50:15 -0500

    Collision Functionality

---
+++ Collision.h
@@ -0,0 +1,37 @@
+#pragma once
+
+/* 
+	We define a template to detect when two game objects collide, the reference types are for 
+	the member of left,right,top,bottom, making it practical for ball and paddle to use.
+*/
+
+	
+
+template <typename T1, typename T2>
+	bool isIntersecting(const T1& mA, const T2& mB) noexcept
+	{
+		return mA.right() >= mB.left() && mA.left() <= mB.right() &&
+			mA.bottom() >= mB.top() && mA.top() <= mB.bottom();
+	}
+
+
+	/*executed every game frame, this function will check if a paddle & ball are colliding,
+    if they are it will resolve collision by making the ball go up in 
+	the opposite direction of where its collided*/
+
+	void solvePaddleBallCollision(const Paddle& mPaddle, Ball& mBall) noexcept
+	{
+		if (!isIntersecting(mPaddle, mBall))
+		{
+			return;
+		}
+
+		//upward direction
+
+		mBall.velocity.y = -Ball::defVelocity;
+
+		//if the ball's center is to the left of paddle's center, it will move left,
+		//otherwise it will move right
+
+		mBall.velocity.x = mBall.x() < mPaddle.x() ? -Ball::defVelocity : Ball::defVelocity;
+	}

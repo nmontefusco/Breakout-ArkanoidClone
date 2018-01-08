3ac20dfe24e299ae7dc3ef7fceedd17089b01c95
Nicholas Montefusco <nmontefusco@yahoo.com>
2018-01-08 13:48:33 -0500

    Update Collision.h

--- Collision.h
+++ Collision.h
@@ -2,7 +2,7 @@
 
 /* 
 	We define a template to detect when two game objects collide, the reference types are for 
-	the member of left,right,top,bottom, making it accessible for ball and paddle to use.
+	the member of left,right,top,bottom, making it practical for ball and paddle to use.
 */
 
 	
@@ -35,3 +35,45 @@ template <typename T1, typename T2>
 
 		mBall.velocity.x = mBall.x() < mPaddle.x() ? -Ball::defVelocity : Ball::defVelocity;
 	}
+
+	/* We need to find out from what direction the ball hit the brick, and respond accordingly.
+	*/
+	void solveBrickBallCollision(Brick& mBrick, Ball& mBall) noexcept
+	{
+	
+		if (!isIntersecting(mBrick, mBall))
+		{
+			return;
+		}
+		
+		mBrick.Isdestroyed = true;
+
+		float overlapLeft = mBall.right() - mBrick.left();
+		float overlapRight = mBrick.right() - mBall.left();
+		float overlapTop = mBall.bottom() - mBrick.top();
+		float overlapBottom = mBrick.bottom() - mBall.top();
+
+		// If  the left overlap is smaller than the
+		// right  we can safely assume the ball hit the brick from the left.
+
+		bool ballFromLeft(std::abs(overlapLeft) < std::abs(overlapRight));
+
+		// We apply the same idea for top/bottom collisions.
+		bool ballFromTop(std::abs(overlapTop) < std::abs(overlapBottom));
+
+		//store the minimum overlaps for the X and Y axes.
+		float minOverlapX = ballFromLeft ? overlapLeft : overlapRight ;
+		float minOverlapY = ballFromTop ? overlapTop : overlapBottom ;
+		
+		//based on the response, we can simulate "realistic" reaction to collision
+		if (std::abs(minOverlapX) < std::abs(minOverlapY))
+		{
+			mBall.velocity.x = ballFromLeft ? -Ball::defVelocity : Ball::defVelocity;
+		}
+		else
+		{
+			mBall.velocity.y = ballFromTop ? -Ball::defVelocity : Ball::defVelocity;
+		}
+
+	}
+
